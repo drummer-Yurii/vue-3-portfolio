754fcf6cbb4a959adb78d6b34426675b8f38531e
Yurii Rybachok <drummer2009@ukr.net>
2025-03-11 10:39:36 +0200

    add counter section

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import { defineAsyncComponent } from 'vue';
 
 const Navbar = defineAsyncComponent(() => import('@/components/layout/Navbar.vue'));
 const HeroSection = defineAsyncComponent(() => import('@/components/layout/HeroSection.vue'));
+const Counter = defineAsyncComponent(() => import('@/components/layout/Counter.vue'));
 </script>
 
 <template>
   <div class="bg-[#D4EBF8] dark:bg-[#080326] min-h-screen">
     <Navbar />
     <HeroSection />
+    <Counter />
   </div>
 </template>
