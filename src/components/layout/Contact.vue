18ae2660d741857d7fd5f1e713cc1a8f676348d6
Yurii Rybachok <drummer2009@ukr.net>
2025-03-11 19:53:59 +0200

    create reusable input component

---
+++ src/components/layout/Contact.vue
@@ -0,0 +1,12 @@
+<script setup>
+import SectionHeader from '@/components/UI/SectionHeader.vue';
+</script>
+
+<template>
+    <section class="mt-32" id="contact">
+        <SectionHeader title="Contact Me" />
+        <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+            <form class="space-y-8"></form>
+        </div>
+    </section>
+</template>
