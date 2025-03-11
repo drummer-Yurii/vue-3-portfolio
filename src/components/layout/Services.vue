c26dc2a56564c3755d0909ad14b16dfd77bf33ed
Yurii Rybachok <drummer2009@ukr.net>
2025-03-11 11:58:17 +0200

    create reusable section header

---
+++ src/components/layout/Services.vue
@@ -0,0 +1,9 @@
+<script setup>
+import SectionHeader from '@/components/UI/SectionHeader.vue';
+</script>
+
+<template>
+    <section id="services" class="relative text-white mt-32">
+        <SectionHeader title="My Services" />
+    </section>
+</template>
